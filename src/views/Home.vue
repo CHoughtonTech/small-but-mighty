9cdcc5b775d4ceca5e8d9e9967bf50c20ea866ae
Oxman285 <fatman_cah@hotmail.com>
2021-06-25 15:53:22 -0600

    Daily updates added
    added daily updates, this is a test.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <img src="../../public/baby.png" /><br/>
+    Lucas James Houghton<br/>
+    6/19/2021 10:22 AM<br/>
+    3 lbs 6 oz <br/>
+    15 inches<br/>
+    <!-- <BaseDailyInfo :update=update></BaseDailyInfo>
+    <BaseDailyInfo :update=secondUpdate></BaseDailyInfo> -->
+    <BaseDailyInfo v-for="u in weekUpdates" :key="u.id" :update="u"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import BaseDailyInfo from "@/components/BaseDailyInfo";
 
 export default {
-  name: "Home",
   components: {
-    HelloWorld,
+    BaseDailyInfo: BaseDailyInfo
   },
+  computed: {
+    weekUpdates() {
+      return this.$store.getters.getUpdatesByWeek(0);
+    }
+  },
+  data() {
+    return {
+      updates: [],
+      update: {
+        id: 0,
+        date: "6/19/2021",
+        details: "Born Today! It's cold out here!",
+        weight: 1540,
+        url: "https://i.imgur.com/veaZVZ0.jpeg",
+        additionalDetails: "Luke was born after complications due to pre-eclampsia."
+      },
+      secondUpdate: {
+        id: 0,
+        date: "6/25/2021",
+        details: "Stayed off oxygen, and gained one whole ounce today! Mom and Dad are happy with my growth!",
+        weight: 1280,
+        url: "https://i.imgur.com/veaZVZ0.jpeg",
+        additionalDetails: "Luke's health has been doing great, and he has managed to stay off of oxygen as well as IV fluids. He will now start gaining some weight, which we already saw with his 1 oz gain today. The doctors are optimistic on his growth, and caution that setbacks are still a possibility with any preemie."
+      }
+    }
+  }
 };
 </script>
