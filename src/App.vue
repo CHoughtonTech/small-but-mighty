4c5583619c249782039699243bbc544fe7e9a9eb
Oxman285 <fatman_cah@hotmail.com>
2021-06-26 01:00:45 -0600

    Removed Link
    Removed About link until further info is entered

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link>
     </div>
     <img class="title-img" src="@/assets/main_ljh.jpg" /><br/>
     <h3>Lucas James Houghton</h3>
